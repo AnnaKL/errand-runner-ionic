e411edb3331ba0ebe9b2de633e7f0021d32b347e
Anna <anna.klimas@gmail.com>
2015-07-08 16:32:52 +0100

    Patch request is working

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -20,7 +20,8 @@ appCtrl.controller('DashCtrl', function($scope, $http, $state, $window, Tasks) {
       }).then(
         function(res) {
           window.localStorage['auth_token'] = (res.data.user.auth_token);
-          window.localStorage['user_id'] = (res.data.user.id)
+          window.localStorage['user_id'] = (res.data.user.id);
+          window.localStorage['username'] = (res.data.username)
           $state.go('tab.map')
         },
         function(err) {
@@ -32,30 +33,48 @@ appCtrl.controller('DashCtrl', function($scope, $http, $state, $window, Tasks) {
       $state.go('tab.map')
 
     }  
-
+  $scope.username = window.localStorage['username']
   $scope.user_id = window.localStorage['user_id']
 
    ionic.Platform.ready(function() {
-  $scope.allTasks= []
+    $scope.allTasks= []
 
-     $scope.userTasks = function(){
-      $http.get('https://evening-plains-3275.herokuapp.com/users/' + $scope.user_id + '/tasks', {
-      headers: {
-                 'Authorization': window.localStorage['auth_token']
-               }
-               }).
-    success(function(data, status, headers, config) {
-      for (var i = 0; i < data.users.length; i++) {
-        $scope.allTasks.push(data.users[i])
+       $scope.userTasks = function(){
+        $http.get('https://evening-plains-3275.herokuapp.com/users/' + $scope.user_id + '/tasks', {
+        headers: {
+                   'Authorization': window.localStorage['auth_token']
+                 }
+                 }).
+      success(function(data, status, headers, config) {
+        for (var i = 0; i < data.users.length; i++) {
+          $scope.allTasks.push(data.users[i])
+        }
+       }).
+      error(function(data, status, headers, config) {
+      })
+
       }
-      console.log($scope.allTasks)
-    }).
-    error(function(data, status, headers, config) {
-    })
+      $scope.userTasks();
 
-    }
-    $scope.userTasks();
   })
+
+
+    $scope.removeTask = function(taskIndex){
+      var token = window.localStorage['auth_token']
+      console.log(token)
+      var taskId = this.$parent.item.id
+      var url = 'https://evening-plains-3275.herokuapp.com/users/' + $scope.user_id + '/tasks/' + taskId
+     
+      $http.patch(url, 
+                      {"task":{"open": true}}, 
+                      {headers: {"Authorization": token}}
+                        );
+                      }
+      
+      
+
+  // {"task": {"open": false}}
+
 
 
    // Tasks = $scope.allTasks
