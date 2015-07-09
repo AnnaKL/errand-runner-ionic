0a1a69a556ee6861479ca5310895e139014c7d74
Anna <anna.klimas@gmail.com>
2015-07-09 18:11:39 +0100

    Autocomplete removed

--- www/js/autocompleteController.js
+++ www/js/autocompleteController.js
@@ -119,61 +119,6 @@ $scope.taskData = {}
         $scope.showError()
         console.log(res);
       });
-  }
-
-    ionic.Platform.ready(function(){
-    autocomplete = new google.maps.places.Autocomplete(
-      /** @type {HTMLInputElement} */
-      (document.getElementById('autocomplete')), {
-        types: ['geocode']
-      })
-     google.maps.event.addListener(autocomplete, 'place_changed', function() {
-      // fillInAddress();
-    })
-
-     autocomplete2 = new google.maps.places.Autocomplete(
-      /** @type {HTMLInputElement} */
-      (document.getElementById('autocomplete2')), {
-        types: ['geocode']
-      })
-     google.maps.event.addListener(autocomplete, 'place_changed', function() {
-
-    })
+    }
   })
-
-  $scope.geolocate = function() {
-    console.log("geolocate")
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function(position) {
-        var geolocation = new google.maps.LatLng(
-          position.coords.latitude, position.coords.longitude);
-        var circle = new google.maps.Circle({
-          center: geolocation,
-          radius: position.coords.accuracy
-        });
-        // autocomplete.setBounds(circle.getBounds());
-      });
-    }
-  }
-
-  function fillInAddress() {
-    // Get the place details from the autocomplete object.
-    var place = autocomplete.getPlace();
-
-    for (var component in componentForm) {
-      document.getElementById(component).value = '';
-      document.getElementById(component).disabled = false;
-    }
-    console.log(place.address_components)
-      // Get each component of the address from the place details
-      // and fill the corresponding field on the form.
-    for (var i = 0; i < place.address_components.length; i++) {
-      var addressType = place.address_components[i].types[0];
-      if (componentForm[addressType]) {
-        var val = place.address_components[i][componentForm[addressType]];
-        document.getElementById(addressType).value = val;
-      }
-    }
-  }
-})
 })
